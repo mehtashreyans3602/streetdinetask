6864c5dfa523eeab108f1fcdf889b3df05fb702f
Shreyans <103171711+mehtashreyans3602@users.noreply.github.com>
2024-01-11 21:53:34 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,19 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header/Header'
 import { useSelector } from 'react-redux'
+import LaunchComp from '@/components/Launches/Launches'
+import Footer from '@/components/Footer/Footer'
+import LandingPageComponent from '@/components/LandingPage/LandingPageComponent'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const darkMode = useSelector((state)=>state.darkMode.darkMode)
   return (
-    <div className={`${darkMode ? ('text-white bg-black'):('text-black bg-white')} h-screen w-full`}>
+    <div className={`${darkMode ? ('text-white bg-black '):('text-black bg-white border-black')} h-screen w-full overflow-auto`}>
     <Header/>
+    {/* <LaunchComp/> */}
+    <LandingPageComponent/>
+    <Footer/>
     </div>
   )
 }
